897ddd9c5de65b40a0d2832bc4e4ad9ac5e753fb
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-07-26 12:54:53 -0400

    Fixed typo in category-test

---
+++ server/db/models/category.js
@@ -0,0 +1,14 @@
+'use strict';
+var Sequelize = require('sequelize');
+
+var db = require('../_db');
+
+module.exports = db.define('category', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    }
+});
