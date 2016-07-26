02f0b7f6df4c01dafc47355822c31ad4ed3da6a9
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-07-26 12:07:11 -0400

    created product model and specs, specs passing

---
+++ server/db/models/product.js
@@ -0,0 +1,30 @@
+'use strict';
+var _ = require('lodash');
+var Sequelize = require('sequelize');
+
+var db = require('../_db');
+
+//testing
+module.exports = db.define('product', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    inventory: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,        
+        validate: {
+            min: 0
+        }
+    }
+});
