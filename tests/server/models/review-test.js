4f6bbbfce6162dee5eb146838458ded0c4827850
Stephanie Adams <adamsstephaniem@gmail.com>
2016-07-27 15:53:26 -0400

    fix merge conflicts

--- tests/server/models/review-test.js
+++ tests/server/models/review-test.js
@@ -5,6 +5,7 @@ var Sequelize = require('sequelize');
 
 var db = require('../../../server/db');
 
+
 var Review = require('../../../server/db/models/review.js')
 
 describe('Review model', function () {
@@ -25,6 +26,7 @@ describe('Review model', function () {
     it('Stars # must be less than 6', function () {
         return Review.build({stars: 7, content: 'This is terrible. 1234567890.'})
         .validate()
+
         .then(function(result){
             expect(result).to.not.exist;
         })
@@ -36,11 +38,13 @@ describe('Review model', function () {
     it('Stars # must be greater than 0', function () {
         return Review.build({stars: 0, content: 'This is just awful. 1234567890.'})
         .validate()
+
         .then(function(result){
             expect(result).to.not.exist;
         })
         .catch(function(err){
             expect(err).to.exist;
+
             expect(err.actual.errors[0].message).to.equal('Validation min failed')
         })
     });
@@ -48,12 +52,12 @@ describe('Review model', function () {
        return Review.build({stars: 4, content: 'Bad.'})
         .validate()
         .then(function(result){
+
             expect(result).to.not.exist;
         })
         .catch(function(err){
             expect(err).to.exist;
             expect(err.actual.errors[0].message).to.equal('Validation len failed')
-
         })
     });
 });
