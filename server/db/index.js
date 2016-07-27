6c1a1dcbb511e5ad9ff71d72c1163ae478778028
Emily Gok Lucas <eclucas@calpoly.edu>
2016-07-27 15:42:14 -0400

    Added missing /

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,6 @@ module.exports = db;
 
 var User = require('./models/user');
 var Order = require('./models/order');
-var Review = require('.models/review');
-var Product = require('.models/product');
-var Category = require('.models/category');
+var Review = require('./models/review');
+var Product = require('./models/product');
+var Category = require('./models/category');
