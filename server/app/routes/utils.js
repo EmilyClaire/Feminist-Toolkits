7b26a1cb580e383affda8d837ce091aab7a74c75
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-07-28 16:44:59 -0400

    updated ensureAdmin

--- server/app/routes/utils.js
+++ server/app/routes/utils.js
@@ -7,11 +7,12 @@ var ensureAuthenticated = function (req, res, next) {
 };
 
 var ensureAdmin = function (req, res, next) {
-    if (req.isAdmin()) {
-        next();
+    if (req.user&&req.user.isAdmin) {
+           next();
     } else {
         res.status(401).end();
     }
 };
 
+//router.param
 module.exports={ensureAuthenticated: ensureAuthenticated, ensureAdmin: ensureAuthenticated}
