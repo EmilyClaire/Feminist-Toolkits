e25c2486915617134ca4117de7aaebfda506eef8
Stephanie Adams <adamsstephaniem@gmail.com>
2016-07-28 15:52:08 -0400

    Added routing to individual api route files

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,11 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
+router.use('/products', require('./products'));
+router.use('/orders', require('./orders'));
+router.use('/users', require('./users'));
+router.use('/categories', require('./categories'));
+router.use('/reviews', require('./reviews'));
 
 // Make sure this is after all of
 // the registered routes!
