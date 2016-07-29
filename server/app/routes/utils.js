c8fcc4ec82a52180a1ca09321924db754301a67d
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-07-29 00:32:32 -0400

    fixed ensureAdmin, added ensureAppropriateUser

--- server/app/routes/utils.js
+++ server/app/routes/utils.js
@@ -1,4 +1,5 @@
 var ensureAuthenticated = function (req, res, next) {
+    console.log('ensuring authenticated')
     if (req.isAuthenticated()) {
         next();
     } else {
@@ -7,12 +8,20 @@ var ensureAuthenticated = function (req, res, next) {
 };
 
 var ensureAdmin = function (req, res, next) {
+    console.log('ENSURING ADMIN')
+    console.log(req.user);
     if (req.user&&req.user.isAdmin) {
+            console.log('passing, cuz')
+            console.log(req.user);
            next();
     } else {
         res.status(401).end();
     }
 };
 
-//router.param
-module.exports={ensureAuthenticated: ensureAuthenticated, ensureAdmin: ensureAuthenticated}
+var ensureAppropriateUser=function(req,resource){
+    /*NOT MIDDLEWARE. For usage example, see 'router.get('/:id'...' in orders.js*/
+    return req.session.passport.user===resource.userId;
+}
+
+module.exports={ensureAuthenticated: ensureAuthenticated, ensureAdmin: ensureAdmin, ensureAppropriateUser: ensureAppropriateUser}
