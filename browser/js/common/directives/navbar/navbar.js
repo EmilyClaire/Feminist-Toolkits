9a1c554d91473460a6ffd082498e5681519bbad7
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-08-02 16:58:03 -0400

    skeleton

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -10,7 +10,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
                 { label: 'Home', state: 'home' },
                 { label: 'About', state: 'about' },
                 { label: 'Products', state: 'products' },
-                { label: 'Members Only', state: 'membersOnly', auth: true }
+                { label: 'My Account', state: 'account', auth: true }
             ];
 
             scope.user = null;
